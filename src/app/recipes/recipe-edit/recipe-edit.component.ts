be8bcd3ab9648f3f257bb389285dc89bd81297f4
ilya.trofimov <trofimov@sednor.net>
2022-03-05 00:13:27 +0200

    Edit recipe form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,38 +1,56 @@
-import { Component, OnChanges, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import RecipeService from "../recipe.service";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, NgForm } from "@angular/forms";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.scss']
 })
-export class RecipeEditComponent implements OnInit, OnChanges {
-  @ViewChild('form') editForm : NgForm
+export class RecipeEditComponent implements OnInit {
 
   id: number
+  recipeForm: FormGroup
   editMode: boolean = false
-  disableSubmitButton : boolean = true
-  answer: string = ""
 
-  constructor(private route: ActivatedRoute, private recipeService : RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {
+  }
+
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      if(params){
-        if(params.id){
+      if (params) {
+        if (params.id) {
           this.id = +params.id
           this.editMode = true
         }
       }
     })
+    this.initForm()
   }
-  ngOnChanges(){
-    this.disableSubmitButton = this.editForm.valid
+
+  private initForm() {
+    let recipeName = "еые"
+    let imagePath = "3131"
+    let recipeDescription = "ывфвф"
+    if (this.editMode) {
+      const editedRecipe: Recipe = this.recipeService.getRecipeById(this.id)
+      recipeName = editedRecipe.name
+      recipeDescription = editedRecipe.description
+      imagePath = editedRecipe.imagePath
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(imagePath),
+      'description': new FormControl(recipeDescription)
+    })
   }
-  onSubmitForm(){
-    console.log(this.editForm)
+
+
+  onSubmitForm() {
+    console.log(this.recipeForm)
   }
 
 }
