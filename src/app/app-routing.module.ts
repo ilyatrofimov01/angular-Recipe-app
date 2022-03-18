c997ec4c75d16afedaa04c2ad75ea1588386905e
ilya.trofimov <trofimov@sednor.net>
2022-03-18 12:23:04 +0200

    Added Lazy-Load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { NgModule } from "@angular/core";
 import { UnAuthorizedComponent } from "./un-authorized/un-authorized.component";
 
 const appRoutes: Routes = [
   {path: "", redirectTo: "/recipes", pathMatch: "full"},
-  {path: "recipes", loadChildren: () => import("./recipes/recipes.module").then(mod=>mod.RecipesModule)},
-  {path: "shopping-list", loadChildren: () => import("./shopping-list/shopping-list.module").then(mod=>mod.ShoppingListModule)},
-  {path: "auth", loadChildren: () => import("./auth/auth.module").then(mod=>mod.AuthModule)},
+  {path: "recipes", loadChildren: () => import("./recipes/recipes.module").then(mod => mod.RecipesModule)},
+  {
+    path: "shopping-list",
+    loadChildren: () => import("./shopping-list/shopping-list.module").then(mod => mod.ShoppingListModule)
+  },
+  {path: "auth", loadChildren: () => import("./auth/auth.module").then(mod => mod.AuthModule)},
   {path: "not-found", component: NotFoundComponent},
   {path: "unAuthorized", component: UnAuthorizedComponent}
   //{path: "**", redirectTo: "not-found"}
@@ -15,7 +18,9 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {
+      preloadingStrategy: PreloadAllModules
+    })
   ],
   exports: [RouterModule]
 })
